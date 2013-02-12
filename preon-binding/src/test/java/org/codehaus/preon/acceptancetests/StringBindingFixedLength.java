e701f26bdc9fdcf2878882a80f7ccdf9a7454ee5
Wilfred Springer <wilfredspringer@gmail.com>
2013-02-12 10:07:29 +0100

    Fixing license headers, removing file that should have never been checked in.

--- preon-binding/src/test/java/org/codehaus/preon/acceptancetests/StringBindingFixedLength.java
+++ preon-binding/src/test/java/org/codehaus/preon/acceptancetests/StringBindingFixedLength.java
@@ -1,3 +1,35 @@
+/**
+ * Copyright (C) 2009-2010 Wilfred Springer
+ *
+ * This file is part of Preon.
+ *
+ * Preon is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * Preon is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Preon; see the file COPYING. If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
 package org.codehaus.preon.acceptancetests;
 
 import org.codehaus.preon.DecodingException;
@@ -59,23 +91,23 @@ public class StringBindingFixedLength {
     private static void assertCanDecodeUTF8FixedLengthString() throws DecodingException {
         final byte[] input = {
                 (byte)0x54,                         // 1 UTF-8 code unit: T
-                (byte)0xC3, (byte)0x9F,             // 2 UTF-8 code units: ß
-                (byte)0xE6, (byte)0x9D, (byte)0xB1  // 3 UTF-8 code units: 東
+                (byte)0xC3, (byte)0x9F,             // 2 UTF-8 code units: ??
+                (byte)0xE6, (byte)0x9D, (byte)0xB1  // 3 UTF-8 code units: ???
         };
         final ClassWithFixedLengthUTF8StringOfSixBytes result = decodeObjectFromInput(ClassWithFixedLengthUTF8StringOfSixBytes.class, input);
 
-        assertThat(result.str, is("Tß東"));
+        assertThat(result.str, is("T?????"));
     }
 
     private static void assertCanDecodeUTF16BEFixedLengthString() throws DecodingException {
         final byte[] input = {
                 (byte)0x00, (byte)0x54, // 1 UTF-8 code unit: T
-                (byte)0x00, (byte)0xDF, // 1 UTF-16 code unit: ß
-                (byte)0x67, (byte)0x71  // 1 UTF-16 code unit: 東
+                (byte)0x00, (byte)0xDF, // 1 UTF-16 code unit: ??
+                (byte)0x67, (byte)0x71  // 1 UTF-16 code unit: ???
         };
         final ClassWithFixedLengthUTF16BEStringOfSixBytes result = decodeObjectFromInput(ClassWithFixedLengthUTF16BEStringOfSixBytes.class, input);
 
-        assertThat(result.str, is("Tß東"));
+        assertThat(result.str, is("T?????"));
     }
 
     private static void assertCanDecodeStringWithSuccessfulMatch() throws DecodingException {
--- preon-binding/src/test/java/org/codehaus/preon/acceptancetests/StringBindingNullTerminated.java
+++ preon-binding/src/test/java/org/codehaus/preon/acceptancetests/StringBindingNullTerminated.java
@@ -1,3 +1,35 @@
+/**
+ * Copyright (C) 2009-2010 Wilfred Springer
+ *
+ * This file is part of Preon.
+ *
+ * Preon is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * Preon is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Preon; see the file COPYING. If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
 package org.codehaus.preon.acceptancetests;
 
 import org.codehaus.preon.DecodingException;
@@ -53,25 +85,25 @@ public class StringBindingNullTerminated {
     private static void assertCanDecodeUTF8NullTerminatedString() throws DecodingException {
         final byte[] input = {
                 (byte)0x54,                         // 1 UTF-8 code unit: T
-                (byte)0xC3, (byte)0x9F,             // 2 UTF-8 code units: ß
-                (byte)0xE6, (byte)0x9D, (byte)0xB1, // 3 UTF-8 code units: 東
+                (byte)0xC3, (byte)0x9F,             // 2 UTF-8 code units: ??
+                (byte)0xE6, (byte)0x9D, (byte)0xB1, // 3 UTF-8 code units: ???
                 (byte)0x00                          // Null termination
         };
         final ClassWithNullTerminatedUTF8String result = decodeObjectFromInput(ClassWithNullTerminatedUTF8String.class, input);
 
-        assertThat(result.str, is("Tß東"));
+        assertThat(result.str, is("T?????"));
     }
 
     private static void assertCanDecodeUTF16BENullTerminatedString() throws DecodingException {
         final byte[] input = {
                 (byte)0x00, (byte)0x54, // 1 UTF-16 code unit: T
-                (byte)0x00, (byte)0xDF, // 1 UTF-16 code unit: ß
-                (byte)0x67, (byte)0x71, // 1 UTF-16 code unit: 東
+                (byte)0x00, (byte)0xDF, // 1 UTF-16 code unit: ??
+                (byte)0x67, (byte)0x71, // 1 UTF-16 code unit: ???
                 (byte)0x00, (byte)0x00  // Null termination
         };
         final ClassWithNullTerminatedUTF16BEString result = decodeObjectFromInput(ClassWithNullTerminatedUTF16BEString.class, input);
 
-        assertThat(result.str, is("Tß東"));
+        assertThat(result.str, is("T?????"));
     }
 
     public static class ClassWithNullTerminatedUTF16BEString {
